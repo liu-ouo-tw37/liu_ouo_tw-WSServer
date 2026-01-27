a124accd29687a8abf30be82066d9c51097e4709
liu-ouo-tw37 <biqiuryan@gmail.com>
2026-01-27 22:50:23 +0800

    Delete liu_ouo_tw的WSSerer directory

--- liu_ouo_tw的WSSerer/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  controlWSS: (action, settings) => ipcRenderer.send("control-wss", { action, settings }),
-  onStatusUpdate: (callback) => ipcRenderer.on("status-update", (event, status) => callback(status)),
-  log: (callback) => ipcRenderer.on("log", (event, message) => callback(message)),
-  getSettings: () => ipcRenderer.invoke("get-settings"),
-});
